deada3aab142c438b7b0eef9befaead4d72f00fc
ralsalloum <ramisalloum12@hotmail.com>
2020-12-23 01:00:36 +0200

    Re-created for insert data issues

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProductFixtures extends Fixture implements FixtureGroupInterface
+{
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 500000; $i++)
+        {
+            $product = new Product();
+
+            $product->setName('Product ' . $i);
+            $product->setPrice($i . ' SYP');
+            $product->setImage(100000 + $i);
+            $product->setSupplier((500000 - $i));
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['group1'];
+    }
+}
